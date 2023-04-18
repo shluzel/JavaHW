ace1748353c4525ce0c24e71ad046fbd28217ed6
Darya <dasha.890@yandex.ru>
2023-04-18 22:53:14 +0500

    task 03

--- task01.java
+++ task01.java
@@ -1,7 +1,5 @@
 //Вычислить n-ое треугольного число (сумма чисел от 1 до n)
-/**
- * task01
- */
+
 import java.util.Scanner;
 
 public class task01 {
---
+++ task03.java
@@ -0,0 +1,30 @@
+//Вывести все простые числа от 1 до 1000 (числа, которые делятся только на 1 и на себя без остатка)
+
+import java.util.Scanner;
+
+public class task03 {
+    public static void main(String[] args) {
+        System.out.println("Введите число N:");
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        System.out.println("Простые числа до N: ");
+        for (int i = 1; i <= N; i++) {
+            System.out.print(simpleNumbers(i));
+        }
+        in.close();
+    }
+    static String simpleNumbers (int Number){
+        int sum = 0;
+        for (int i = 1; i < Number; i++) {
+            if (Number % i == 0) {
+                sum = sum +1;
+            }
+        }
+        if (sum == 1) {
+            return Integer.toString(Number) + " ";
+        }
+        else {
+            return "";
+        }
+    }
+}
